5a9c2eff201cd1e718aba39d57cea01b530c9707
William Martin <97082653+Acceleration-Industries@users.noreply.github.com>
2024-02-07 21:58:18 -0800

    First Update

--- rangers_shop/blueprints/auth/routes.py
+++ rangers_shop/blueprints/auth/routes.py
@@ -3,85 +3,82 @@ from werkzeug.security import check_password_hash
 from flask_login import login_user, logout_user 
 
 
-#internal import 
-from rangers_shop.models import User, db 
-from rangers_shop.forms import RegisterForm, LoginForm
+# internal imports
+from rangers_shop.models import User, db # grabbing our class so we can instantiate User objects
+from rangers_shop.forms import RegisterForm, LoginForm 
 
 
+# instantiate our Blueprint
+auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
-#instantiate our blueprint
-auth = Blueprint('auth', __name__, template_folder='auth_templates') #template folder is navigating to where html files are located
 
-
-@auth.route('/signup', methods=['GET', 'POST'])
+# our routes for sign up, sign in, logout
+@auth.route('/signup', methods=['GET', 'POST']) #GET request to SEE the form, POST request to POST data to our database 
 def signup():
-
-    #we need to instantiate our form as an object in order to use it
+    
+    # instantiate our RegisterForm class
     registerform = RegisterForm()
-
+    
     if request.method == 'POST' and registerform.validate_on_submit():
-        #grabbing our data from our form after it was submitted 
-        first_name = registerform.first_name.data 
+        #grab our data from the submitted form
+        first_name = registerform.first_name.data
         last_name = registerform.last_name.data 
-        username = registerform.username.data 
-        email = registerform.email.data 
+        username = registerform.username.data
+        email = registerform.email.data
         password = registerform.password.data 
-
-        print(email, password, username)
-
-        #query into database to check for username &/or email that already exists
-
+        
+        print('Register Form', email, password) # testing that we are getting data from the form
+        
+        # query into our database to check if the username and/or email already exists 
+        
         if User.query.filter(User.username == username).first():
-            flash(f"Username already exists. Please Try Again", category='warning')
+            flash(f"Username {username} already exists. Please Try Again!", category='warning')
             return redirect('/signup')
         if User.query.filter(User.email == email).first():
-            flash("Email already exists. Please Try Again", category='warning')
+            flash(f"Email {email} already exists. Please Try Again!", category='warning')
             return redirect('/signup')
         
-        #we can now instantiate a new user object!!!
+        # instantiating our user object from User class
+        user = User(username, email, password, first_name, last_name)
         
-        user = User(username, email, password, first_name, last_name) 
-
-        #now we can add our user object to our database
-        db.session.add(user) #think of this like "git add ."
-        db.session.commit() #think of this like "git commit"
-
-
+        # we can add this object to our database
+        db.session.add(user)
+        db.session.commit()
+        
+        
         flash(f"You have successfully registered user {username}", category='success')
         return redirect('/signin')
     
-    return render_template('sign_up.html', form=registerform )
-
-
+    return render_template('sign_up.html', form=registerform ) #passing loginform object as form in html 
+    
 @auth.route('/signin', methods=['GET', 'POST'])
 def signin():
-
-    #instantiate my loginform
+    
+    # instantiate LoginForm
     loginform = LoginForm()
-
-
+    
     if request.method == 'POST' and loginform.validate_on_submit():
-        #grab our data from the form
         email = loginform.email.data
         password = loginform.password.data 
-        print("login info", email, password)
-
-        user = User.query.filter(User.email == email).first()
-
-
-        if user and check_password_hash(user.password, password): 
-            #this is where we are using our UserMixin class & load_user() function
-            login_user(user) #this is now saved as current_user everywhere in our application 
-            flash(f"Successfully logged in {email}", category='success') 
-            return redirect('/') #so if a user successfully logs in, we are going to send them home 
+        
+        print('Login Form', email, password)
+        
+        # query to find the user with the same email
+        user = User.query.filter(User.email == email).first()  # same as SELECT * FROM user WHERE email = email 
+        
+        if user and check_password_hash(user.password, password):
+            # if this comes back we have validated our user and using UserMixin & load_user() function to load user
+            login_user(user) # this makes this user our current_user
+            flash(f"Successfully logged in {email}", category='success')
+            return redirect('/')
         else:
-            flash("Invalid Email or Password, Please Try Again", category='warning')
+            flash("Invalid Email and/or Password, Please Try Again!", category='warning')
             return redirect('/signin')
     
-    return render_template('sign_in.html', form=loginform )
-
-
+    # if its a 'GET' request we need to display this form to the user
+    return render_template('sign_in.html', form=loginform ) #passing loginform object as form in html 
+    
 @auth.route('/logout')
 def logout():
-    logout_user()
+    logout_user() #current_user is now None
     return redirect('/')
